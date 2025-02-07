3cf890627ee567022a09881c4129cc3e8d4233ed
Nikhil sharm <purohitwork2002@gmail.com>
2025-02-07 10:10:17 +0530

    second

--- Database/db.js
+++ Database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB=async ()=>{
     try {
-        await mongoose.connect(process.env.Local_URL);
+        await mongoose.connect(process.env.Global_URl);
         console.log('MongoDB Connected')
     } catch (error) {
         console.log('error occured',error)
